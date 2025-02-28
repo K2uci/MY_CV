66ea37f068b65f203bf6af1f4774737929561071
K2'sUCI <148832097+K2uci@users.noreply.github.com>
2025-02-28 04:07:26 +0000

    Update 2025#1

--- script.js
+++ script.js
@@ -45,8 +45,8 @@ window.onscroll = () => {
 
 function descargarArchivo() {
     const enlace = document.createElement('a');
-    enlace.href = './my_cv.pdf'; // Reemplaza con el nombre de tu archivo
-    enlace.download = './my_cv.pdf'; // Reemplaza con el nombre de tu archivo
+    enlace.href = './cv.docx'; // Reemplaza con el nombre de tu archivo
+    enlace.download = './cv.docx'; // Reemplaza con el nombre de tu archivo
     enlace.click();
 }
 
