3688955390cf2dca4642a119b84425c5317f4050
K2uci <elieserhs999omega@gmail.com>
2025-12-27 17:10:48 -0500

    Update 12/2025 Ciber

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-
 //icon navbar
 let menuIcon = document.querySelector('#menu-icon')
 let navbar = document.querySelector('.navbar')
 
 menuIcon.onclick = () => {
-	menuIcon.classList.toggle('bx-x');
-	navbar.classList.toggle('active');
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
 }
 
 //scroll settigns
@@ -14,39 +13,39 @@ let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
 
-	sections.forEach(sec =>{
-		let top = window.scrollY;
-		let offset = sec.offsetTop - 100;
-		let height = sec.offsetHeight;
-		let id = sec.getAttribute('id');
+    sections.forEach(sec =>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
 
-		if(top >= offset && top < offset + height) {
-			navLinks.forEach(links => {
-				links.classList.remove('active');
-				document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
-			})
-			sec.classList.add('show-animate')
-		} 
-		else {
-			sec.classList.remove('show-animate')
-		}
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
+            })
+            sec.classList.add('show-animate')
+        } 
+        else {
+            sec.classList.remove('show-animate')
+        }
 
-	});
+    });
 
-	let header = document.querySelector('header');
+    let header = document.querySelector('header');
 
-	header.classList.toggle('sticky', window.scrollY > 100)
+    header.classList.toggle('sticky', window.scrollY > 100)
 
-	// borrar todo la hacer click
+    // borrar todo la hacer click
 
-	menuIcon.classList.remove('bx-x');
-	navbar.classList.remove('active');
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 }
 
 function descargarArchivo() {
     const enlace = document.createElement('a');
-    enlace.href = './cv.docx'; // Reemplaza con el nombre de tu archivo
-    enlace.download = './cv.docx'; // Reemplaza con el nombre de tu archivo
+    enlace.href = './CV_Ciber_en_12_2025.pdf'; // Usa tu archivo PDF real
+    enlace.download = 'Elieser_Hernandez_Pentester_CV.pdf';
     enlace.click();
 }
 
@@ -59,5 +58,5 @@ function sendEmail() {
 
     var emailBody = `Nombre: ${name}\nCorreo Electrónico: ${email}\nTelefono: ${phone}\nAsunto: ${topic}\nMensaje: ${message}`;
     console.log(emailBody)
-    window.location.href = `mailto:astroreal031@gmail.com?subject=${topic}&body=${encodeURIComponent(emailBody)}`;
+    window.location.href = `mailto:astrorealo31@gmail.com?subject=${topic}&body=${encodeURIComponent(emailBody)}`;
 }
